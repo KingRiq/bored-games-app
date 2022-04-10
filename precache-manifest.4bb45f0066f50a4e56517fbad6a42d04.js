edd69c43846928a9ef72fe0d391f7343b32a0e11
Eriq Hampton <eriqrhampton@yahoo.com>
2022-04-10 12:43:12 -0500

    Updates

---
+++ precache-manifest.4bb45f0066f50a4e56517fbad6a42d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "881a67bcba03e3a32244",
+    "url": "/bored-game-app/static/css/main.90dd508c.chunk.css"
+  },
+  {
+    "revision": "881a67bcba03e3a32244",
+    "url": "/bored-game-app/static/js/main.881a67bc.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/bored-game-app/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "5da7fdb947786506adde",
+    "url": "/bored-game-app/static/js/2.5da7fdb9.chunk.js"
+  },
+  {
+    "revision": "8944a09c48ccd9766da0",
+    "url": "/bored-game-app/static/js/runtime~main.8944a09c.js"
+  },
+  {
+    "revision": "53cb75f921d728be9e9b74a043dde231",
+    "url": "/bored-game-app/index.html"
+  }
+];
